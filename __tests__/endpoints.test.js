19fff10481cf5a1ca26da599969aa0b9a31ceb26
Erich Sauer <erich.sauer@gmail.com>
2021-02-18 16:08:18 -0800

    all tests go!

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -9,7 +9,32 @@ it('should return all plants',
             results: plants
         };
 
-        const response = await request.get('/pokemon');
+        const response = await request.get('/plants');
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toEqual(expectation);
+
+        done();
+    });
+
+it('should return all plants',
+    async (done) => {
+        const plant = {
+            id: 1,
+            name: 'Crested Fluffel',
+            image: 'crested-fluffel.jpg',
+            description: 'Very rare fruiting body with feather-like pollination whisps.',
+            fragrant: true,
+            safetyNotes: 'Sniff in moderation.',
+            price: 10
+        };
+        
+        const expectation = {
+            results: plant 
+        };
+
+        const response = await request.get('/plants/1');
 
         expect(response.status).toBe(200);
 
